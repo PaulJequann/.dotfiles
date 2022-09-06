fe8a1d114f76d1098b325bd4b2279e0f61a83aa5
PJ <paul@pauljequann.com>
2022-09-06 08:47:10 -0400

    Revert "Revert "nvim config changes - autopairs cmp nvim tree updated""
    This reverts commit 4b951dcd9de3dd779c79c2f4ba384e4c2299f923.

--- nvim/.config/nvim/lua/pj/lualine.lua
+++ nvim/.config/nvim/lua/pj/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup {
     options = {
         icons_enabled = true,
-        theme = 'tokyonight',
+        theme = 'auto',
         disabled_filetypes = {'NvimTree'}
     },
     sections = {
